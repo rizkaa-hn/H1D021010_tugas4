bd185fdf35f0c2023cc2596041accaf3c09e7d71
rizkaa-hn <102272398+rizkaa-hn@users.noreply.github.com>
2023-10-02 21:07:40 +0700

    Add files via upload
    ci

---
+++ ci/app/Models/MRegistrasi.php
@@ -0,0 +1,7 @@
+<?php 
+namespace App\Models;
+use CodeIgniter\Model;
+class MRegistrasi extends Model{
+    protected $table = 'member';
+    protected $allowedFields =['nama','email','password'];
+}
